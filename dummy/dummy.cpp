daca64d4238d46640ef51a6121b61a5cd0b9f0f9
Marius Ioan Orban <marius.orban99@gmail.com>
2018-10-20 14:39:21 +0300

    Added a dummy target so CMake can build V-EZ separately and before Lava2D without being the biggest pain in the ass

---
+++ dummy/dummy.cpp
@@ -0,0 +1,5 @@
+/**
+ * Dummy C++ file for a dummy target used when building just V-EZ 
+ * I couldn't get CMake to build that *before* Lava2D any other way
+ * A moment of silence for the hours spent trying to fix this crap
+ */
